2d8cf0083d49b03607ab4d16babfa63098d72972
teashown <teashown@gmail.com>
2013-07-03 13:21:42 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bangbom::Application.config.secret_token = '9c7f2a32a3322025fadaec42d422574f685b3642c5dc13b0c463105c82f7bc9bf6dedc4e098cb0e23712e4702ae64104c0e758b68bd428092754ca8553e96959'
